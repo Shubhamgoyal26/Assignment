e0584463c11f4ef6180b18f0c58e20b1e9a2d249
Shubham Goyal <53435671+Shubhamgoyal26@users.noreply.github.com>
2021-02-14 16:21:02 +0530

    Create stairs.c

---
+++ stairs.c
@@ -0,0 +1,19 @@
+// Drawing stairs using binary
+
+#include <stdio.h>
+
+int main(void){
+    int stairs;
+
+    printf("%s", "Enter number of stairs you want: ");
+    scanf("%d", &stairs);
+
+    for(int i = 0; i < stairs; i++){
+        for(int j = 0; j <= i; j++){
+            printf("01");
+        }
+        printf("\n");
+    }
+
+ return 0;
+}
